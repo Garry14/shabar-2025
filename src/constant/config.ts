da4f0678f5afc613cb9e19a03a92293d6e749d29
garrydev <garryandi@badr-interactive.com>
2025-03-11 14:38:24 +0700

    changing heading

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'Sharing Bareng Developer',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+    'Sharing Ramadhan 1446H Bareng Developer',
+  url: 'https://sharing-bareng-developer.vercel.app',
 };
