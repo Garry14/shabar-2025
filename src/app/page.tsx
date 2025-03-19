974e2884a7bd1d52eb66ebd10998641501f1c912
garrydev <garryandi@badr-interactive.com>
2025-03-19 15:04:25 +0700

    config button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Head from 'next/head';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '@/lib/env';
 
 import NextIcon from '@/components/icons/NextIcon';
@@ -9,9 +9,20 @@ import NextIcon from '@/components/icons/NextIcon';
 import { dataPage } from '@/constant/data';
 
 export default function HomePage() {
-  const [page, setPage] = useState<{ content: string; effect: string }>(
-    dataPage[0],
-  );
+  const [page, setPage] = useState<{
+    content: string;
+    effect: string;
+    arrangement: string;
+  }>(dataPage[0]);
+  const [appearButton, setAppearButton] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAppearButton(true);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [page]);
+
   return (
     <main>
       <Head>
@@ -20,38 +31,38 @@ export default function HomePage() {
       <section className='bg-white'>
         <div className='layout relative py-12 flex flex-col items-center justify-center min-h-screen'>
           <div className='font-thin text-6xl text-center'>{page.content}</div>
-          <div className='mt-9 w-full flex justify-center'>
-            <button
-              onClick={() => {
-                const index = dataPage.indexOf(page);
-                if (index === 0) {
-                  setPage(dataPage[dataPage.length - 1]);
-                } else {
-                  setPage(dataPage[index - 1]);
-                }
-              }}
-              className='bg-green-500 hover:bg-green-300 w-12 h-12 mr-2'
-            >
-              <div className='text-white rotate-180'>
-                <NextIcon />
-              </div>
-            </button>
-            <button
-              onClick={() => {
-                const index = dataPage.indexOf(page);
-                if (index === dataPage.length - 1) {
-                  setPage(dataPage[0]);
-                } else {
-                  setPage(dataPage[index + 1]);
-                }
-              }}
-              className='bg-green-500 hover:bg-green-300 w-12 h-12'
-            >
-              <div className='text-white '>
-                <NextIcon />
-              </div>
-            </button>
-          </div>
+          {appearButton && (
+            <div className='mt-9 w-full flex justify-center'>
+              {['end', 'body'].includes(page.arrangement) && (
+                <button
+                  onClick={() => {
+                    const index = dataPage.indexOf(page);
+                    setPage(dataPage[index - 1]);
+                    setAppearButton(false);
+                  }}
+                  className='bg-green-500 hover:bg-green-300 w-12 h-12 mr-2'
+                >
+                  <div className='text-white rotate-180'>
+                    <NextIcon />
+                  </div>
+                </button>
+              )}
+              {['start', 'body'].includes(page.arrangement) && (
+                <button
+                  onClick={() => {
+                    const index = dataPage.indexOf(page);
+                    setPage(dataPage[index + 1]);
+                    setAppearButton(false);
+                  }}
+                  className='bg-green-500 hover:bg-green-300 w-12 h-12'
+                >
+                  <div className='text-white'>
+                    <NextIcon />
+                  </div>
+                </button>
+              )}
+            </div>
+          )}
         </div>
       </section>
     </main>
--- src/constant/data.ts
+++ src/constant/data.ts
@@ -1,18 +1,21 @@
 export const dataPage = [
-  { content: "Assalaamu'alaikum", effect: 'fade' },
+  { content: "Assalaamu'alaikum", effect: 'fade', arrangement: 'start' },
   {
     content:
       'Bicara tentang kepribadian seorang manusia, sudah barangpasti pribadi yang disukai oleh orang-orang secara sepakat adalah pribadi yang bermanfaat untuk sesamanya, pribadi yang tidak egois, pribadi yang suka menolong.',
     effect: 'fade',
+    arrangement: 'body',
   },
   {
     content:
       'Berbicara tentang tolong menolong yang tentunya harus dalam hal kebaikan, ini sangat ditekankan dalam Islam, sehingga sangat banyak sekali hadits-hadits yang menjanjikan bagi orang-orang yang suka menolong, bermanfaat bagi sesamanya, dalam lingkungan apapun. Apakah itu di keluarga, atau di masyarakat, atau di dalam circle persahabatan.',
     effect: 'fade',
+    arrangement: 'body',
   },
   {
     content:
       'Bahkan Rosul lebih suka menolong meringankan beban orang lain, jika dibandingkan itikaf di "Masjid ini" yakni Masjid Nabawi dalam 1 bulan.',
     effect: 'fade',
+    arrangement: 'end',
   },
 ];
