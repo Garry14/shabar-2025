b60a0400495c7e57fb43a3864626db93de037dfe
garrydev <garryandi@badr-interactive.com>
2025-03-19 13:31:10 +0700

    page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import Head from 'next/head';
-import * as React from 'react';
+import { useState } from 'react';
 import '@/lib/env';
 
+import { dataPage } from '@/constant/data';
+
 export default function HomePage() {
+  const [page, setPage] = useState<{ content: string; effect: string }>(
+    dataPage[0],
+  );
   return (
     <main>
       <Head>
@@ -12,7 +17,22 @@ export default function HomePage() {
       </Head>
       <section className='bg-white'>
         <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-          Test
+          <div className='text-2xl font-bold'>{page.content}</div>
+          <div className='mt-4'>
+            <button
+              onClick={() => {
+                const index = dataPage.indexOf(page);
+                if (index === dataPage.length - 1) {
+                  setPage(dataPage[0]);
+                } else {
+                  setPage(dataPage[index + 1]);
+                }
+              }}
+              className='btn btn-primary'
+            >
+              Next
+            </button>
+          </div>
         </div>
       </section>
     </main>
