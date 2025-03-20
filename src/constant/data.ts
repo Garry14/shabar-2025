945667353bf14b17bc02251f24a45dd574d847cc
garrydev <garryandi@badr-interactive.com>
2025-03-20 11:24:22 +0700

    fixing data

--- src/constant/data.ts
+++ src/constant/data.ts
@@ -1,26 +1,86 @@
 export const dataPage = [
-  { content: "Assalaamu'alaikum", arrangement: 'start' },
+  {
+    content:
+      "Assalaamu'alaikum Warahmatullaahi wabarokaatuh, semoga teman2 yang kesini bisa lebih mudah bacanya",
+    arrangement: 'start',
+  },
+  {
+    content: 'Bismillah...',
+    arrangement: 'body',
+  },
+  {
+    content: 'Judulnya : Itikaf Nyambi Kerja',
+    arrangement: 'body',
+  },
   {
     content:
       'Bicara tentang kepribadian seorang manusia, sudah barangpasti pribadi yang disukai oleh orang-orang secara sepakat adalah pribadi yang bermanfaat untuk sesamanya, pribadi yang tidak egois, pribadi yang suka menolong.',
+    arrangement: 'body',
+  },
+  {
+    content:
+      'Berbicara tentang tolong menolong yang tentunya harus dalam hal kebaikan, ini sangat ditekankan dalam Islam, sehingga sangat banyak sekali hadits-hadits yang menjanjikan bagi orang-orang yang suka menolong,',
+    arrangement: 'body',
+  },
+  {
+    content:
+      'Dan juga Allah menjanjikan bagi siapa yang menjadi orang bermanfaat, yakni menolong mempermudah urusan orang lain, maka dia akan dipermudah juga ketika di Mahsyar,',
+    arrangement: 'body',
+  },
+  {
+    content:
+      'yang mana kita tau bahwa siapapun di Mahsyar, baik yang shaleh maupun yang kafir, mereka akan merasakan kesulitan di hari tersebut, merasakan ketakutan, mencekam,',
+    arrangement: 'body',
+  },
+  {
+    content:
+      'sampai Nabi Ibrahim Khalil Allah pun berkata : "Allah sedang murka yang mana belum pernah semurka ini sebelumnya, dan tidak akan semurka ini setelahnya"',
+    arrangement: 'body',
+  },
+  {
+    content:
+      "Tentang janji Allah ini tercantum dalam Hadits ke-36 dari Hadits Araba'in nya Imam An-Nawawi, teman2 silahkan browse aja atau silahkan buka buku haditsnya jika punya,",
+    arrangement: 'body',
+  },
+  {
+    content:
+      'atau silahkan load lagi hafalannya jika sudah menghafal hadits ini,',
 
     arrangement: 'body',
   },
   {
     content:
-      'Berbicara tentang tolong menolong yang tentunya harus dalam hal kebaikan, ini sangat ditekankan dalam Islam, sehingga sangat banyak sekali hadits-hadits yang menjanjikan bagi orang-orang yang suka menolong, bermanfaat bagi sesamanya, dalam lingkungan apapun. Apakah itu di keluarga, atau di masyarakat, atau di dalam circle persahabatan.',
+      '"Siapa yang menyelesaikan kesulitannya seorang Mukmin dari kesuliatan2 dunia, niscaya Allah akan memudahkannya dari kesulitan2 di Akhirat" (HR Muslim)',
 
     arrangement: 'body',
   },
   {
     content:
-      'Bahkan Rosul lebih suka menolong meringankan beban orang lain, jika dibandingkan itikaf di "Masjid ini" yakni Masjid Nabawi dalam 1 bulan.',
+      'Di samping itu, Rosul lebih suka menolong meringankan beban orang lain, jika dibandingkan itikaf di "Masjid ini" yakni Masjid Nabawi, dalam 1 bulan.',
 
     arrangement: 'body',
   },
   {
     content:
-      'Sumber : <a href="https://muslim.or.id/86060-ibadah-semakin-bermanfaat-semakin-berpahala.html" class="text-blue-600">Muslim.or.id -> Ibadah semakin bermanfaat semakin berpahala</a>',
+      'Tentunya dari segi Fiqih pun, kita akan mendahuluhan kewajiban Aqad dibandingkan dengan Itikaf full yang mana itu hukumnya tidak wajib.',
+
+    arrangement: 'body',
+  },
+  {
+    content:
+      'Jadi bagi teman2 Braders/Badriyah yang belum dapat kesempatan menjalani itikaf full, tetaplah semangat dalam menjalani kerja memberikan maslahat kepada orang2 / client2 / user2 kita.',
+
+    arrangement: 'body',
+  },
+  {
+    content:
+      "Wallaahu a'lam. Hadaniyallaahu wa iyyaakum, Wassalaamu'alaykum Warahmatullaahi wabarokaatuh",
+
+    arrangement: 'body',
+  },
+  {
+    content:
+      'Silahkan baca juga : <a href="https://muslim.or.id/86060-ibadah-semakin-bermanfaat-semakin-berpahala.html" class="text-blue-600">Muslim.or.id -> Ibadah semakin bermanfaat semakin berpahala</a>',
     arrangement: 'end',
   },
 ];
